1b54b181c3ad56e7175ce8608f4f46eb5487f422
Adam R Moss <adamrmoss@gmail.com>
2022-09-26 01:12:34 -0400

    Correct number partition algorithm

--- src/number-partition.ts
+++ src/number-partition.ts
@@ -17,11 +17,14 @@ function getNumberPartitions(n: number): NumberPartition[]
         return [[1]];
     }
 
-    const previousPartitions = getNumberPartitions(n - 1);
-    const newPartitions: NumberPartition[] = [[n], ...previousPartitions.flatMap(partition => [[1, ...partition], [...partition, 1]])];
-
     const partitions = new SetWithEquality<NumberPartition>(jsonEquals);
-    newPartitions.forEach(partition => partitions.add(partition));
+    for (let i = 1; i < n; i++)
+    {
+        const previousPartitions = getNumberPartitions(n - i);
+        const newPartitions: NumberPartition[] = [[n], ...previousPartitions.flatMap(partition => [[i, ...partition], [...partition, i]])];
+
+        newPartitions.forEach(partition => partitions.add(partition));
+    }
 
     return [...partitions];
 }
